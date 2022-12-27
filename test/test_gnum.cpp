e45d73747fa7d4587cfb88f7a81b26253ec75b2c
Christian Gann <guo.guo.gan@gmail.com>
2022-12-27 14:03:35 +0800

    completed metal test standalone

--- test/test_gnum.cpp
+++ test/test_gnum.cpp
@@ -15,6 +15,11 @@
 #include <stdlib.h>
 #include <time.h>
 
+#ifdef __APPLE__
+#include <Foundation/Foundation.hpp>
+#include <Metal/Metal.hpp>
+#endif
+
 #include "gnum.hpp"
 
 static void
@@ -69,9 +74,8 @@ test_perf()
 {
   printf("test perf:\n");
 
-
   int i = 0;
-  double v1[] = {
+  float v1[] = {
     -0.094491, -0.443977,  0.313917,
     -0.490796, -0.229903,  0.065460,
      0.072921,  0.172246, -0.357751,
@@ -81,29 +85,88 @@ test_perf()
      0.181755,  0.088268,  0.277574,
     -0.055335,  0.491792,  0.263102
   };
-  double v2[] = {
+  float v2[] = {
      0.023074,  0.479901,  0.432148,  0.375480, -0.364732, -0.119840,  0.266070, -0.351000,
     -0.368008,  0.424778, -0.257104, -0.148817,  0.033922,  0.353874, -0.144942,  0.130904,
      0.422434,  0.364503,  0.467865, -0.020302, -0.423890, -0.438777,  0.268529, -0.446787
   };
 
-  double* rs;
-  gn::metal_init();
+  float* rs;
 
   gn::mat mat0(10000, 300);
   gn::mat mat1(10000, 300);
   float* res = (float*) calloc(10000 * 300, sizeof(double));
   mat0.rand(1);
   mat1.rand(1);
-  for (i = 0; i < 1000; i++)
+  for (i = 0; i < 100; i++)
   {
 //    gn::mat mat0(8, 3, v1);
 //    gn::mat mat1(3, 8, v2);
-//    gn::mat res = mat0 + mat1;
+    gn::mat res1 = mat0 + mat1;
 
-    gn::metal_mat_add(mat0.values(), mat1.values(), 10000 * 300, res);
+//    gn::metal_mat_add(mat0.values(), mat1.values(), 10000 * 300, res);
   }
 }
+
+#ifdef __APPLE__
+static void
+test_metal()
+{
+  printf("test metal:\n");
+
+  uint i;
+  uint size = 10000 * 300;
+  gn::mat mat0(10000, 300);
+  gn::mat mat1(10000, 300);
+  float* res = (float*) calloc(10000 * 300, sizeof(double));
+  mat0.rand(1);
+  mat1.rand(1);
+
+  NS::AutoreleasePool* pool = NS::AutoreleasePool::alloc()->init();
+  MTL::Device* device = MTL::CreateSystemDefaultDevice();
+
+  NS::Error* error;
+  MTL::Buffer* m1 = device->newBuffer(mat0.values(), size * sizeof(float), MTL::ResourceStorageModeShared);
+  MTL::Buffer* m2 = device->newBuffer(mat1.values(), size * sizeof(float), MTL::ResourceStorageModeShared);
+  MTL::Buffer* rs = device->newBuffer(res, size * sizeof(float), MTL::ResourceStorageModeShared);
+
+  NS::String* libpath = NS::String::string("matop.metallib", NS::UTF8StringEncoding);
+  auto lib = device->newLibrary(libpath, &error);
+  auto fn = NS::String::string("add_arrays", NS::ASCIIStringEncoding);
+  MTL::Function* fun_add = lib->newFunction(fn);
+
+  MTL::ComputePipelineState*    pipeline_state  = device->newComputePipelineState(fun_add, &error);
+  MTL::CommandQueue*            command_queue   = device->newCommandQueue();
+
+
+  MTL::Size grid_size = MTL::Size(size, 1, 1);
+
+  NS::UInteger thread_count = pipeline_state->maxTotalThreadsPerThreadgroup();
+  if (thread_count > size)
+    thread_count = size;
+  MTL::Size thread_group_size = MTL::Size(thread_count, 1, 1);
+
+  for (i = 0; i < 10000; i++)
+  {
+    MTL::CommandBuffer*           command_buffer  = command_queue->commandBuffer();
+    MTL::ComputeCommandEncoder*   compute_encoder = command_buffer->computeCommandEncoder();
+
+    compute_encoder->setComputePipelineState(pipeline_state);
+
+    compute_encoder->setBuffer(m1, 0, 0);
+    compute_encoder->setBuffer(m2, 0, 1);
+    compute_encoder->setBuffer(rs, 0, 2);
+
+    compute_encoder->dispatchThreads(grid_size, thread_group_size);
+    compute_encoder->endEncoding();
+
+    command_buffer->commit();
+    command_buffer->waitUntilCompleted();
+  }
+
+  pool->release();
+}
+#endif
 
 int
 main()
@@ -117,15 +180,19 @@ main()
   long elapsed_nsecs;
 
   clock_gettime(CLOCK_REALTIME, &before);
-
   test_perf();
-
-  /* handle connection */
   clock_gettime(CLOCK_REALTIME, &after);
-
   elapsed_nsecs = (after.tv_sec - before.tv_sec) * 1000000000 +
                   (after.tv_nsec - before.tv_nsec);
-
   printf("  elapsed %0.2f ms!\n", (double) elapsed_nsecs / 1000000);
+
+#ifdef __APPLE__
+  clock_gettime(CLOCK_REALTIME, &before);
+  test_metal();
+  clock_gettime(CLOCK_REALTIME, &after);
+  elapsed_nsecs = (after.tv_sec - before.tv_sec) * 1000000000 +
+                  (after.tv_nsec - before.tv_nsec);
+  printf("  elapsed %0.2f ms!\n", (double) elapsed_nsecs / 1000000);
+#endif
   return 0;
 }
